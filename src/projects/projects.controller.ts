1e2810fdafe5c1b3cf8641ffae005a1bc24e3c0e
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-10 21:24:04 -0700

    created a projects service

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private projectsService: ProjectsServicectsService);
+}
