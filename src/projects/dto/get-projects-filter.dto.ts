acc3c8cd0141ddd490fa63c6cf28ae487751999b
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-19 00:56:06 -0700

    validation for get projects with filters

--- src/projects/dto/get-projects-filter.dto.ts
+++ src/projects/dto/get-projects-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { ProjectStatus } from '../project.model';
 
 export class GetProjectsFilterDto {
+  @IsOptional()
+  @IsEnum(ProjectStatus)
   status?: ProjectStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
