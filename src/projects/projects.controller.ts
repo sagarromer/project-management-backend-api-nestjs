8e3e85b3dd13c34c68990871b7498868eb154c2c
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-12-20 15:51:26 -0800

    persisit getting project by id

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -12,38 +12,15 @@ import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectsService } from './projects.service';
 import { UpdateProjectStatusDto } from './dto/update-project-status.dto';
 import { ProjectStatus } from './project-status.enum';
+import { Project } from './project.entity';
 import { GetProjectsFilterDto } from './dto/get-projects-filter.dto';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private projectsService: ProjectsService) {}
 
-  // @Get()
-  // getProjects(@Query() filterDto: GetProjectsFilterDto): Project[] {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.projectsService.getProjectsWithFilters(filterDto);
-  //   } else {
-  //     return this.projectsService.getAllProjects();
-  //   }
-  // }
-  // @Get('/:id')
-  // getProjectById(@Param('id') id: string): Project {
-  //   return this.projectsService.getProjectById(id);
-  // }
-  // @Post()
-  // createProject(@Body() createProjectDto: CreateProjectDto): Project {
-  //   return this.projectsService.createProject(createProjectDto);
-  // }
-  // @Delete('/:id')
-  // deleteProject(@Param('id') id: string): void {
-  //   return this.projectsService.deleteProject(id);
-  // }
-  // @Patch('/:id/status')
-  // updateProjectStatus(
-  //   @Param('id') id: string,
-  //   @Body('status') updateProjectStatusDto: UpdateProjectStatusDto,
-  // ): Project {
-  //   const { status } = updateProjectStatusDto;
-  //   return this.projectsService.updateProjectStatus(id, status);
-  // }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Project> {
+    return this.projectsService.getProjectById(id);
+  }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,58 +2,22 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProjectStatus } from './project-status.enum';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { GetProjectsFilterDto } from './dto/get-projects-filter.dto';
-
+import { ProjectsRepository } from './projects.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
 @Injectable()
 export class ProjectsService {
-  // private projects: Project[] = [];
-  // getAllProjects(): Project[] {
-  //   return this.projects;
-  // }
-  // getProjectById(id: string): Project {
-  //   const found = this.projects.find((project) => project.id === id);
-  //   if (!found) {
-  //     throw new NotFoundException(`Project with ID "${id}" not found`);
-  //   }
-  //   return found;
-  // }
-  // createProject(createProjectDto: CreateProjectDto): Project {
-  //   const { title, description } = createProjectDto;
-  //   const project: Project = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: ProjectStatus.OPEN,
-  //   };
-  //   this.projects.push(project);
-  //   return project;
-  // }
-  // deleteProject(id: string): void {
-  //   const found = this.getProjectById(id);
-  //   this.projects = this.projects.filter((project) => project.id !== found.id);
-  // }
-  // updateProjectStatus(id: string, status: ProjectStatus) {
-  //   const project = this.getProjectById(id);
-  //   project.status = status;
-  //   return project;
-  // }
-  // getProjectsWithFilters(filterDto: GetProjectsFilterDto): Project[] {
-  //   const { status, search } = filterDto;
-  //   let projects = this.getAllProjects();
-  //   // do something with status
-  //   if (status) {
-  //     projects = projects.filter((project) => project.status === status);
-  //   }
-  //   if (search) {
-  //     projects = projects.filter((project) => {
-  //       if (
-  //         project.title.includes(search) ||
-  //         project.description.includes(search)
-  //       ) {
-  //         return true;
-  //       }
-  //       return false;
-  //     });
-  //   }
-  //   return projects;
-  // }
+  constructor(
+    @InjectRepository(ProjectsRepository)
+    private projectsRepository: ProjectsRepository,
+  ) {}
+  async getProjectById(id: string): Promise<Project> {
+    const found = await this.projectsRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Project with ID "${id}" not found`);
+    }
+
+    return found;
+  }
 }
