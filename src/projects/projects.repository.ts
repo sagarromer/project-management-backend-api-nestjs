b828e9ed9c03873408ea3db9221ad2beeebb4d06
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-12-23 16:42:18 -0800

    getting all projects completed

--- src/projects/projects.repository.ts
+++ src/projects/projects.repository.ts
@@ -1,5 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { GetProjectsFilterDto } from './dto/get-projects-filter.dto';
 import { ProjectStatus } from './project-status.enum';
 import { Project } from './project.entity';
 
@@ -16,5 +17,24 @@ export class ProjectsRepository extends Repository<Project> {
 
     await this.save(project);
     return project;
+  }
+  async getProjects(filterDto: GetProjectsFilterDto): Promise<Project[]> {
+    const { status, search } = filterDto;
+
+    const query = this.createQueryBuilder('project');
+
+    if (status) {
+      query.andWhere('project.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(project.title) LIKE LOWER(:search) OR LOWER(project.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const projects = await query.getMany();
+    return projects;
   }
 }
